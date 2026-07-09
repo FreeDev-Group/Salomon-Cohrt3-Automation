ac81683811d5449d311f64f843612c92d7440f67
alainbahanep <alainbahanep@gmail.com>
2026-07-09 15:30:53 +0300

    Complete Create Account E2E test and report

--- Alain/cypress.config.js
+++ Alain/cypress.config.js
@@ -1,10 +1,21 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+
+  // Record videos during cypress run
+  video: true,
+
+  // Save screenshots here
+  screenshotsFolder: 'cypress/screenshots',
+
+  // Save videos here
+  videosFolder: 'cypress/videos',
+
   e2e: {
     baseUrl: 'https://student.michaelkentburns.com',
 
-    // 👇 IMPORTANT : chemin standard Cypress
+    // Cypress test files location
     specPattern: 'cypress/e2e/**/*.cy.js'
   }
+
 })
