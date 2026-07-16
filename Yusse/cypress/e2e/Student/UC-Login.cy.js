961b6b42dea93183fdae9dc73a07cd4682cbde2d
Yusse-ai <yussecelestin@gmail.com>
2026-07-16 22:30:11 +0200

    #7

--- Yusse/cypress/e2e/Student/UC-Login.cy.js
+++ Yusse/cypress/e2e/Student/UC-Login.cy.js
@@ -1,17 +1,72 @@
 import 'cypress-mochawesome-reporter/register';
 /// <reference types="Cypress" />
-
+// login test case
 describe('Login', () => {
 
   it('should allow a user to log in', () => {
-  cy.visit('https://student.michaelkentburns.com/');
-  cy.contains('User').click();
-  cy.contains('Login').click();
-   cy.get('#user_login').type('yussecelestin@gmail.com');
-  cy.get('#user_pass').type('13579013@#Yc');
-  cy.contains('Remember Me').click({ force: true });
-  cy.get('#wp-submit').click();
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.get('#user_login').type('yussecelestin@gmail.com');
+    cy.get('#user_pass').type('13579013@#Yc');
+    cy.contains('Remember Me').click({ force: true });
+    cy.get('#wp-submit').click();
+  });
 
+  // invalid password
+  it('should display an error message for invalid password', () => {
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.get('#user_login').type('yussecelestin@gmail.com');
+    cy.get('#user_pass').type('wrongpassword');
+    cy.get('#wp-submit').click();
+    cy.contains('Invalid username or password.');
+  });
+
+
+  // invalid username
+  it('should display an error message for invalid username', () => {
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.get('#user_login').type('invaliduser');
+    cy.get('#user_pass').type('13579013@#Yc');
+    cy.get('#wp-submit').click();
+    cy.contains('Invalid username or password.');
+  });
+
+  // forgot password
+  it('should allow a user to reset their password', () => {
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.contains('Lost your password?').click();
+    cy.get('#user_login').type('yussecelestin@gmail.com');
+    cy.get('#wp-submit').click();
+    // assert that a reset confirmation message is shown
+    cy.contains('Check your email').should('exist');
+  });
+
+  //login attempt with empty fields
+  it('should display an error message for empty fields', () => {
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.get('#wp-submit').click();
+    cy.contains('Please enter a username and password.').should('exist');
+  });
+
+  //logout test case
+  it('should allow a user to log out', () => {
+    cy.visit('https://student.michaelkentburns.com/');
+    cy.contains('User').click();
+    cy.contains('Login').click();
+    cy.get('#user_login').type('yussecelestin@gmail.com');
+    cy.get('#user_pass').type('13579013@#Yc');
+    cy.contains('Remember Me').click({ force: true });
+    cy.get('#wp-submit').click();
+    cy.contains('Log Out').click();
   });
 
 });
